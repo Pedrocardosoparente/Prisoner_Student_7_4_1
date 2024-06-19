1593c9b38e7f2f86fc3787433fc2671df2897849
Aluno <Aluno@10.85.198.36>
2024-06-19 16:36:14 -0300

    7-4

---
+++ src/prisoner_student_7_42/Prisoner_Student_7_42.java
@@ -0,0 +1,15 @@
+public class Prisoner_Student_7_42 {
+
+    //Fields 
+    public String name;
+    public double height;
+    public int sentence;
+    
+    //Constructor
+    
+    
+    //Methods
+    public void think(){
+        System.out.println("I'll have my revenge.");
+    }   
+}
